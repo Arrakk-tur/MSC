2d455567b5edc9c0968678bf3d6485084b117908
Kostiantyn Skliar <skljar.k@gmail.com>
2024-02-18 20:46:05 +0200

    Autocheck 6.6 - 6.11

--- Autocheck/mod06/01.py
+++ Autocheck/mod06/01.py
@@ -71,3 +71,98 @@
 # source = "Autocheck/mod06/tex.txt"
 # output = "Autocheck/mod06/tex_o.txt"
 # sanitize_file(source,output)
+
+# --- 6.6 ---
+
+# def save_applicant_data(source, output):
+
+#     with open(output, "w") as f_out:
+#         for i in source:
+#             f_out.write(f"{i['name']},{i['specialty']},{i['math']},{i['lang']},{i['eng']}\n")
+        
+
+
+# source = [
+#     {
+#         "name": "Kovalchuk Oleksiy",
+#         "specialty": 301,
+#         "math": 175,
+#         "lang": 180,
+#         "eng": 155,
+#     },
+#     {
+#         "name": "Ivanchuk Boryslav",
+#         "specialty": 101,
+#         "math": 135,
+#         "lang": 150,
+#         "eng": 165,
+#     },
+#     {
+#         "name": "Karpenko Dmitro",
+#         "specialty": 201,
+#         "math": 155,
+#         "lang": 175,
+#         "eng": 185,
+#     },
+# ]
+# output = "Autocheck/mod06/stude_o.txt"
+# save_applicant_data(source, output)
+
+# --- 6.7 ---
+
+# def is_equal_string(utf8_string: bytes, utf16_string: bytes):
+#     if utf8_string.decode('utf-8') == utf16_string.decode('utf-16'):
+#         return True
+#     else:
+#         return False
+
+# --- 6.8 ---
+
+# def save_credentials_users(path, users_info: dict):
+#     with open(path, "wb") as f:
+#         for k, v in users_info.items():
+#             f.write(f"{k}:{v}\n".encode())
+
+# users_info = {'andry':'uyro18890D', 'steve':'oppjM13LL9e'}
+# path = "Autocheck/mod06/users.txt"
+# save_credentials_users(path, users_info)
+
+# --- 6.9 ---
+
+# def get_credentials_users(path):
+#     users = []
+#     with open(path, "rb") as f:
+#         for line in f.readlines():
+#             users.append(line.decode().rstrip())
+    
+#     return users
+
+# --- 6.10 ---
+
+# import shutil
+
+
+
+# def create_backup(path, file_name, employee_residence: dict):
+#     file_path = path + "/" + file_name
+#     with open(file_path, "wb") as f:
+#         for k, v in employee_residence.items():
+#             f.write(f"{k} {v}\n".encode())
+
+#     backup_folder = shutil.make_archive("backup_folder", "zip", path)
+
+#     return backup_folder
+
+# employee_residence = {'Michael': 'Canada', 'John':'USA', 'Liza': 'Australia'}
+# file_name = "file_b"
+# path = "Autocheck/mod06/arch"
+
+# create_backup(path, file_name, employee_residence)
+
+# --- 6.11 ---
+
+# import shutil
+
+
+# def unpack(archive_path, path_to_unpack):
+#     shutil.unpack_archive(archive_path, path_to_unpack)
